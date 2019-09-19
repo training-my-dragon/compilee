e7589b20ff6fa5e6d6bb3fc23e1748ee12bcd99a
tarberd <bernardo.mferrari@gmail.com>
2019-09-19 15:27:45 -0300

    Add Assignment and Statements

--- src/lib.rs
+++ src/lib.rs
@@ -6,49 +6,9 @@ lalrpop_mod!(pub calculator);
 #[derive(Debug, Clone)]
 pub enum Token {
     Identifier,
-    Semicolon,
-    Int,
-    Float,
-    String,
-
-    LBracket,
-    RBracket,
-}
-
-pub enum LexicalError {}
-
-use std::str::CharIndices;
-
-pub struct Lexer<'input> {
-    chars: CharIndices<'input>,
-}
-
-impl<'input> Lexer<'input> {
-    pub fn new(input: &'input str) -> Self {
-        Lexer {
-            chars: input.char_indices(),
-        }
-    }
-}
-
-pub type Spanned<Tok, Loc, Error> = Result<(Loc, Tok, Loc), Error>;
-
-impl<'input> Iterator for Lexer<'input> {
-    type Item = Spanned<Token, usize, LexicalError>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match self.chars.next() {
-                Some((i, ' ')) => return Some(Ok((i, Token::Semicolon, i + 1))),
-                None => return None,
-                _ => continue,
-            }
-        }
-    }
 }
 
 #[test]
 fn calculator() {
-    let lexer = Lexer::new("int banana[];");
-    calculator::ProgramParser::new().parse(lexer).ok();
+    calculator::ProgramParser::new().parse("{ int __banana55; banana = 55; banana = 77; }").unwrap();
 }
