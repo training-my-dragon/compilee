37c2a1bbd9e885b5a74800a2f65335499e781572
tarberd <bernardo.mferrari@gmail.com>
2019-10-08 15:45:24 -0300

    Add command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+use compilee::parser;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+
+    args.remove(0);
+
+    println!("Compiling files: {:?}", args);
+
+    for arg in args {
+
+        let mut file = File::open(arg).expect("failed to open file.");
+
+        let mut file_string = String::new();
+
+        file.read_to_string(&mut file_string).unwrap();
+
+        parser::ProgramParser::new().parse(&file_string).unwrap();
+    }
 }
