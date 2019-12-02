eb29936e3be893146eedc69df0f8b1e5ff7f027d
tarberd <bernardo.mferrari@gmail.com>
2019-12-02 17:01:44 -0300

    ast draft

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,6 @@ pub mod ast;
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub parser);
 
-#[derive(Debug, Clone)]
-pub enum Token {
-    Identifier,
-}
-
 #[test]
 fn calculator() {
     parser::ProgramParser::new().parse("{ int __banana55; banana = 55; banana = 77; }").unwrap();
